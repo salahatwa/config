55aa16dbd10c454533ecfa73189ec2eb2d5e6883
salah atwa <salahsayedatwa@gmail.com>
2024-06-25 13:28:01 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@ echo "Deployment has been started";
 
 cd /home/kayany_website/itways ;
 
+rm -rf docker-compose.yml &&curl -O https://raw.githubusercontent.com/salahatwa/config/main/docker-compose.yml ;
+
 sudo docker compose down ;
 
 sudo docker rmi atwa4/crowdfunding-api:latest &&sudo docker rmi atwa4/crowdfunding-panel:latest ;
