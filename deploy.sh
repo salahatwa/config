6765e3cdcc7f50215cf858783197d8d5d911d5d1
salah atwa <salahsayedatwa@gmail.com>
2024-08-09 17:18:10 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,9 @@ sudo rm -rf docker-compose.yml;
 
 sudo rm -rf nginx.conf;
 
-sudo curl -O https://raw.githubusercontent.com/salahatwa/config/main/docker-compose.yml;
+curl -O https://raw.githubusercontent.com/salahatwa/config/main/docker-compose.yml;
 
-sudo curl -O https://raw.githubusercontent.com/salahatwa/config/main/nginx.conf;
+curl -O https://raw.githubusercontent.com/salahatwa/config/main/nginx.conf;
 
 sudo docker compose down ;
 
