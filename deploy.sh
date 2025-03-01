f3ee5e9c7b38b6be58831d15d98cdd566279657b
salah atwa <salahsayedatwa@gmail.com>
2025-03-01 15:49:53 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ curl -O https://raw.githubusercontent.com/salahatwa/config/main/nginx.conf;
 
 sudo docker compose down ;
 
-sudo docker rmi atwa4/crowdfunding-api:latest &&sudo docker rmi atwa4/crowdfunding-panel:latest && sudo docker rmi nginx:latest ;
+sudo docker rmi atwa4/crowdfunding-api:latest &&sudo docker rmi atwa4/crowdfunding-panel:latest && &&sudo docker rmi atwa4/crowdown-panel:latest && sudo docker rmi nginx:latest ;
 
 sudo docker compose pull&&sudo docker compose build ;
 
