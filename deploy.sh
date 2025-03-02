8036ebd840815c09c5498eca5306c4160ef80509
salah atwa <salahsayedatwa@gmail.com>
2025-03-02 12:45:57 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "Deployment has been started";
 
 cd /home/kayany_website/itways ;
 
-sudo chmod 777 /var/run/docker.sock
+sudo chmod 777 /var/run/docker.sock;
 
 sudo docker network create webnet;
 
@@ -16,10 +16,10 @@ curl -O https://raw.githubusercontent.com/salahatwa/config/main/nginx.conf;
 
 sudo docker compose down ;
 
-sudo docker rmi atwa4/crowdfunding-api:latest &&sudo docker rmi atwa4/crowdfunding-panel:latest && sudo docker rmi atwa4/crowdown-panel:latest && sudo docker rmi nginx:latest ;
+sudo docker rmi atwa4/crowdfunding-api:latest &&sudo docker rmi atwa4/crowdfunding-panel:latest && sudo docker rmi nginx:latest ;
 
 sudo docker compose pull&&sudo docker compose build ;
 
 sudo docker compose up -d;
 
-echo "Deployment has been done success";
+echo "Deployment has been done success :)";
