176bfb98459adb86379b90c0bb9ebd94a3ed98e2
salah atwa <salahsayedatwa@gmail.com>
2025-04-29 15:06:28 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,6 @@ sudo chmod 777 /var/run/docker.sock;
 
 sudo docker network create webnet;
 
-sudo rm -rf docker-compose.yml;
-
-sudo rm -rf nginx.conf;
-
-curl -O https://raw.githubusercontent.com/salahatwa/config/main/docker-compose.yml;
-
-curl -O https://raw.githubusercontent.com/salahatwa/config/main/nginx.conf;
-
 sudo docker compose down ;
 
 sudo docker rmi atwa4/crowdfunding-api:latest &&sudo docker rmi atwa4/crowdfunding-panel:latest && sudo docker rmi nginx:latest ;
