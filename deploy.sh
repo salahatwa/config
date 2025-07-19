fcb798abfd13a8fe37bb5a701e3aa47176b80beb
salah atwa <salahsayedatwa@gmail.com>
2025-07-19 11:57:00 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 echo "Deployment has been started";
 
-cd /home/kayany_website/itways/event-core;
+cd /home/kayany_website/itways/itways-ai;
 
 sudo chmod 777 /var/run/docker.sock;
 
@@ -10,13 +10,13 @@ sudo rm -rf docker-compose.yml;
 
 sudo rm -rf nginx.conf;
 
-curl -O https://raw.githubusercontent.com/salahatwa/config/event-core/docker-compose.yml;
+curl -O https://raw.githubusercontent.com/salahatwa/config/itways-ai/docker-compose.yml;
 
-curl -O https://raw.githubusercontent.com/salahatwa/config/event-core/nginx.conf;
+curl -O https://raw.githubusercontent.com/salahatwa/config/itways-ai/nginx.conf;
 
 sudo docker compose down;
 
-sudo docker rmi atwa4/event-core-api:latest && sudo docker rmi nginx:latest ;
+sudo docker rmi atwa4/itways-ai-api:latest && sudo docker rmi nginx:latest ;
 
 sudo docker compose pull && sudo docker compose build ;
 
